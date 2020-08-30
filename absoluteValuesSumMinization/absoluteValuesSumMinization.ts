10f2becdd9f96dabccc0e90b66036850eecc326a
Steamroot <samuelalejandro08x@gmail.com>
2020-08-30 15:09:39 -0400

    Add

--- absoluteValuesSumMinization/absoluteValuesSumMinization.ts
+++
@@ -1,9 +0,0 @@
-function absoluteValuesSumMinimization(a: number[]): number {
-    let x: number = ((a.length - 1)/2);
-    return (a[Math.floor(x)]);
-}
-
-console.log(absoluteValuesSumMinimization([2, 4, 7]));
-console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
-console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
-console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
