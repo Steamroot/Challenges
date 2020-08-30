cc46b333c74ba793ddbb0e1bedff9abd997066f7
Steamroot <samuelalejandro08x@gmail.com>
2020-08-30 15:18:42 -0400

    Add Two Digits

---
+++ addTwoDigits/addTwoDigits.ts
@@ -0,0 +1,12 @@
+function addTwoDigits(n: any): number {
+    const nums = n.toString().split('');
+    
+    return nums.reduce((a: string, b: string) => {
+        return parseInt(a) + parseInt(b);
+    });
+
+    console.log(n);
+
+}
+
+console.log(addTwoDigits(29));
