0661bd58cad71c6f7dfabf2a9fbcf9563d96bc1b
Steamroot <samuelalejandro08x@gmail.com>
2020-09-17 23:35:06 -0400

    Alternating Sums

---
+++ alternatingSums/alternatingSums.ts
@@ -0,0 +1,19 @@
+function alternatingSums(a: number[]): number[] {
+
+    let teamOne = 0;
+    let teamTwo = 0;
+
+    
+    a.forEach((element, index) => {
+        if(index % 2 === 0) {
+            teamOne += element;
+        } else {
+            teamTwo += element;
+        }
+    })
+
+    return [teamOne, teamTwo];
+
+}
+
+console.log(alternatingSums([50, 60, 60, 45, 70]))
