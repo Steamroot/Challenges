27ba709881ffb926dbcbf6d191162de2896125a0
Steamroot <samuelalejandro08x@gmail.com>
2020-09-21 20:59:36 -0400

    Added Are Equally Strong

--- areEquallyStrong/areEquallyStrong.ts
+++ areEquallyStrong/areEquallyStrong.ts
@@ -1,7 +1,23 @@
 function areEquallyStrong(yourLeft: number, yourRight: number, friendsLeft: number, friendsRight: number): boolean {
+    
+    let validity: number = 0;
 
+    if (yourLeft === friendsLeft || yourLeft == friendsRight) {
+        validity += 1;
+    }
+
+    if (yourRight === friendsRight || yourRight === friendsLeft) {
+        validity += 1;
+    }
+
+    if (validity == 2) {
+        return true
+    }
+
+    return false;
+    
 }
 
-// console.log(areEquallyStrong(10, 15, 15, 10))
-// console.log(areEquallyStrong(15, 10, 15, 10))
-// console.log(areEquallyStrong(15, 10, 15, 9))
+console.log(areEquallyStrong(10, 15, 15, 10))
+console.log(areEquallyStrong(15, 10, 15, 10))
+console.log(areEquallyStrong(15, 10, 15, 9))
